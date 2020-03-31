c53c997c0bdf0115131d629d5179032c3de83265
harsha <harsha.baragada@gmail.com>
2020-03-31 02:26:32 +0200

    add new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
+import { LoginComponent } from './feature/login/login.component';
+import { HomeComponent } from './feature/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/generics/header/header.component.ts
+++ src/app/generics/header/header.component.ts
@@ -13,4 +13,8 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  sampleMethod(){
+    alert("sample method");
+  }
+
 }
